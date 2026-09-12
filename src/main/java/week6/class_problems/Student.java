e471381889b3c2b4df795ecb37aa099cc4dd2112
Kavin07exe <kavin8c@gmail.com>
2026-09-12 21:44:29 +0530

    Add Week 6 practice problems

---
+++ src/main/java/week6/class_problems/Student.java
@@ -0,0 +1,39 @@
+package week6.class_problems;
+
+public class Student {
+
+    String name;
+    double attendance;
+
+    static String collegeName =
+            "SRM Institute of Science and Technology";
+
+    static int studentCount = 0;
+
+    public Student(String name, double attendance) {
+
+        this.name = name;
+        this.attendance = attendance;
+
+        studentCount++;
+    }
+
+    static void printCollegeInfo() {
+
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+
+    public static void main(String[] args) {
+
+        Student student1 =
+                new Student("Ravi", 90.0);
+
+        Student student2 =
+                new Student("Anitha", 85.0);
+
+        System.out.println("2 Student objects created");
+
+        Student.printCollegeInfo();
+    }
+}
